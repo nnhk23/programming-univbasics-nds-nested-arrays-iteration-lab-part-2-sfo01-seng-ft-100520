91bde70e4a937260e0ae55bbb5076eeebae42cab
Kim Nguyen <nnhk23@gmail.com>
2020-09-13 03:46:06 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,7 @@ def find_min_in_nested_arrays(src)
  
  while count < src.length do
    number_index = 0 
-   smallest = src[count][1]
+   smallest = 100
    while number_index < src[count].length do
      if src[count][number_index] < smallest
        result << src[count][number_index]
